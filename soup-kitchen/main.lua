74bff5a8962a6fa8cd553867fea2b039b00460d9
Bryan DeGrendel <bryan@degrendel.com>
2016-01-03 15:34:26 -0500

    Load log and settings into Main

--- soup-kitchen/main.lua
+++ soup-kitchen/main.lua
@@ -1,6 +1,13 @@
 -- Soup Kitchen
 -- Bryan DeGrendel (c) 2016
 
+Log = require "log"
+Inspect = require "library.inspect"
+
+local tag = "Main"
+
+Log.info(tag, "Loading...")
+
 function love.load()
 end
 
