e44463cd4da28e9b28b9298d986e9c84160feab0
Bryan DeGrendel <bryan@degrendel.com>
2016-01-03 15:34:38 -0500

    More skeletons for the settings test

--- tests/settings.lua
+++ tests/settings.lua
@@ -2,3 +2,9 @@
 -- Bryan DeGrendel (c) 2016
 
 require "soup-kitchen.settings"
+
+Log = require "soup-kitchen.log"
+Inspect = require "soup-kitchen.library.inspect"
+
+describe("Settings", function()
+end)
