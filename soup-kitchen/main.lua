deee13e5914aa0bb12ee64b585f03e3edbb17435
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 00:18:25 -0500

    Main now creates and calls Core

--- soup-kitchen/main.lua
+++ soup-kitchen/main.lua
@@ -8,12 +8,19 @@ require "library.strict"
 
 Log = require "log"
 
-local tag = "Main"
+local tag, core = "Main", nil
 
-Log.info(tag, "Loading...")
+require "core"
 
 function love.load()
+  Log.info(tag, "Loading...")
+  core = Core()
 end
 
-function love.draw()
-end
+function love.update() core:update() end
+function love.draw() core:draw() end
+
+function love.keypressed(key) core:keypressed(key) end
+function love.keyreleased(key) core:keyreleased(key) end
+function love.mousepressed(x, y, button) core:mousepressed(x, y, button) end
+function love.mousereleased(x, y, button) core:mousereleased(x, y, button) end
