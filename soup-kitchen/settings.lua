eeeba517ace11caaa7a1a2ef369474b0b16bad51
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 00:17:18 -0500

    Initial defaults added to Settings
    Just the magic section's QuickExit settings, for now.

--- soup-kitchen/settings.lua
+++ soup-kitchen/settings.lua
@@ -5,5 +5,15 @@ local tag = "Settings"
 
 Settings = Class("Settings")
 
+local function generate_defaults(root)
+  root.Magic = {
+    QuickExit = {
+      Enabled = true,
+      Key = "escape"
+    }
+  }
+end
+
 function Settings:init()
+  generate_defaults(self)
 end
