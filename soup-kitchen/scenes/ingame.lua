3cabb7791b933048790ffe6128dc24354ce721e5
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 02:35:43 -0500

    Added background drawing to InGame & MainMenu

--- soup-kitchen/scenes/ingame.lua
+++ soup-kitchen/scenes/ingame.lua
@@ -7,6 +7,16 @@ local tag = "InGame"
 
 InGame = Scene:extend("InGame")
 
+function InGame:init(core)
+  InGame.super:init(core)
+  self.Core.Settings.Scenes.InGame = {
+    Images = {
+      Background = "background.png"
+    },
+  }
+  self.Background = self:loadImage("Background")
+end
+
 function InGame:enter()
 end
 
@@ -14,6 +24,7 @@ function InGame:update(dt)
 end
 
 function InGame:draw()
+  love.graphics.draw(self.Background)
 end
 
 function InGame:exit()
--- soup-kitchen/scenes/mainmenu.lua
+++ soup-kitchen/scenes/mainmenu.lua
@@ -7,6 +7,20 @@ local tag = "MainMenu"
 
 MainMenu = Scene:extend("MainMenu")
 
+function MainMenu:init(core)
+  MainMenu.super:init(core)
+  self.Core.Settings.Scenes.MainMenu = {
+    Images = {
+      Background = "background.png"
+    },
+  }
+  self.Background = self:loadImage("Background")
+end
+
+function MainMenu:draw()
+  love.graphics.draw(self.Background)
+end
+
 function MainMenu:keyPressed(key)
   self.Core:changeScene("InGame")
 end
