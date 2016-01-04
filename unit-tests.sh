7bada6bdcaa6a345275ed1e0086f5f2e0eb05e48
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 00:18:40 -0500

    Added shell script to run unit tests

---
+++ unit-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! type "luajit" > /dev/null ; then
+  echo "LuaJIT not found!"
+  exit 1
+fi
+
+if ! type "busted" > /dev/null ; then
+  echo "busted not found!"
+  exit 1
+fi
+
+busted --lua=luajit tests/
