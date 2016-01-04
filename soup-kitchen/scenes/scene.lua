11752e5fc8b291deb40870b8d2693a0bf379c6a9
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 01:03:36 -0500

    Minor code cleanup in Scene

--- soup-kitchen/scenes/scene.lua
+++ soup-kitchen/scenes/scene.lua
@@ -7,37 +7,39 @@ Scene = Class("Scene")
 
 function Scene:init(core)
   self.core = core
-  self.enter_warn = false
-  self.draw_warn = false
-  self.update_warn = false
-  self.exit_warn = false
+  self.OverrideWarn = {
+    Enter = false,
+    Draw = false,
+    Update = false,
+    Exit = false,
+  }
 end
 
 function Scene:enter()
-  if not self.enter_warn then
+  if not self.OverrideWarn.Enter then
     Log.warn(tag, "Scene %s doesn't override :enter()!", self.name)
-    self.enter_warn = true
+    self.OverrideWarn.Enter = true
   end
 end
 
 function Scene:exit()
-  if not self.exit_warn then
+  if not self.OverrideWarn.Exit then
     Log.warn(tag, "Scene %s doesn't override :exit()!", self.name)
-    self.exit_warn = true
+    self.OverrideWarn.Exit = true
   end
 end
 
 function Scene:draw()
-  if not self.draw_warn then
+  if not self.OverrideWarn.Draw then
     Log.warn(tag, "Scene %s doesn't override :draw()!", self.name)
-    self.draw_warn = true
+    self.OverrideWarn.Draw = true
   end
 end
 
 function Scene:update()
-  if not self.update_warn then
+  if not self.OverrideWarn.Update then
     Log.warn(tag, "Scene %s doesn't override :update()!", self.name)
-    self.update_warn = true
+    self.OverrideWarn.Update = true
   end
 end
 
