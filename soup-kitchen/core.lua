00b9e0feabdaa8e024083c4b22ef5159fc0c38d3
Bryan DeGrendel <bryan@degrendel.com>
2016-01-04 01:00:21 -0500

    Basic Scene support

--- soup-kitchen/core.lua
+++ soup-kitchen/core.lua
@@ -3,6 +3,8 @@
 
 require "settings"
 
+require "scenes.mainmenu"
+
 local tag = "Core"
 
 Core = Class("Core")
@@ -10,26 +12,52 @@ Core = Class("Core")
 function Core:init()
   Log.info(tag, "Initializing")
   self.Settings = Settings()
+  self.Scenes = {
+    MainMenu = MainMenu(self)
+  }
+  self.ActiveScene = self.Scenes.MainMenu
+  self.ActiveScene:enter()
+  self.NewScene = nil
 end
 
 function Core:draw()
+  self.ActiveScene:draw()
 end
 
 function Core:update()
+
+  if self.NewScene then
+    self.ActiveScene:exit()
+    self.ActiveScene = self.NewScene
+    self.NewScene = nil
+    self.ActiveScene:enter()
+  end
+
+  self.ActiveScene:update()
 end
 
-function Core:keypressed(key)
+function Core:changeScene(scene)
+  assert(self.Scenes[scene])
+  Log.info(tag, "Switching from %s to %s", self.ActiveScene.name, scene)
+  self.NewScene = self.Scenes[scene]
+end
+
+function Core:keyPressed(key)
   if self.Settings.Magic.QuickExit.Enabled and key == self.Settings.Magic.QuickExit.Key then
     os.exit(true)
   end
+  self.ActiveScene:keyPressed(key)
 end
 
-function Core:keyreleased(key)
+function Core:keyReleased(key)
+  self.ActiveScene:keyReleased(key)
 end
 
-function Core:mousepressed(x, y, button)
+function Core:mousePressed(x, y, button)
+  self.ActiveScene:mousePressed(x, y, button)
 end
 
-function Core:mousereleased(x, y, button)
+function Core:mouseReleased(x, y, button)
+  self.ActiveScene:mouseReleased(x, y, button)
 end
 
--- soup-kitchen/main.lua
+++ soup-kitchen/main.lua
@@ -20,7 +20,7 @@ end
 function love.update() core:update() end
 function love.draw() core:draw() end
 
-function love.keypressed(key) core:keypressed(key) end
-function love.keyreleased(key) core:keyreleased(key) end
-function love.mousepressed(x, y, button) core:mousepressed(x, y, button) end
-function love.mousereleased(x, y, button) core:mousereleased(x, y, button) end
+function love.keypressed(key) core:keyPressed(key) end
+function love.keyreleased(key) core:keyReleased(key) end
+function love.mousepressed(x, y, button) core:mousePressed(x, y, button) end
+function love.mousereleased(x, y, button) core:mouseReleased(x, y, button) end
