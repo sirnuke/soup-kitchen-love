100cfb031008e450523ebe45e69ab8bbc87e5a1f
Bryan DeGrendel <bryan@degrendel.com>
2016-01-06 01:32:44 -0500

    Better range assertions for Coordinate & Position

--- soup-kitchen/world/coordinate.lua
+++ soup-kitchen/world/coordinate.lua
@@ -3,9 +3,13 @@
 
 Coordinate = Class("Position")
 
-function Coordinate:init(x, y)
-  assert(type(x) == "number" and x >= 0)
-  assert(type(y) == "number" and y >= 0)
+function Coordinate:init(core, x, y)
+  assert(Class.isInstance(core, Core))
+  assert(type(x) == "number" and x >= 0 and x < core.Settings.Map.Dimensions.Width,
+    string.format("x:%i is out of range [0..%i)", x, core.Settings.Map.Dimensions.Width))
+  assert(type(y) == "number" and y >= 0 and y < core.Settings.Map.Dimensions.Height,
+    string.format("y:%i is out of range [0..%i)", y, core.Settings.Map.Dimensions.Height))
+  self.Core = core
   self.X = x
   self.Y = y
 end
--- soup-kitchen/world/position.lua
+++ soup-kitchen/world/position.lua
@@ -3,9 +3,15 @@
 
 Position = Class("Position")
 
-function Position:init(x, y)
-  assert(type(x) == "number" and x >= 0)
-  assert(type(y) == "number" and y >= 0)
+function Position:init(core, x, y)
+  assert(Class.isInstance(core, Core))
+  local maxWidth = core.Settings.Map.Dimensions.Width * core.Settings.Map.Tile.Dimensions.Width
+  local maxHeight = core.Settings.Map.Dimensions.Heigth * core.Settings.Map.Tile.Dimensions.Height
+  assert(type(x) == "number" and x >= 0 and x < maxWidth,
+    string.format("x:%i is out of range [0..%i)", x, maxWidth))
+  assert(type(y) == "number" and y >= 0 and y < maxHeight,
+    string.format("y:%i is out of range [0..%i)", y, maxHeight))
+  self.Core = core
   self.X = x
   self.Y = y
 end
