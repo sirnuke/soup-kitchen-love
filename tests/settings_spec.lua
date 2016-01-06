968bc512f66ba1cef9c501d5134aefb1a01d1472
Bryan DeGrendel <bryan@degrendel.com>
2016-01-06 02:08:18 -0500

    Better basic Settings test

--- tests/settings_spec.lua
+++ tests/settings_spec.lua
@@ -3,14 +3,19 @@
 
 require "tests.setup"
 
-before_each(function ()
-  require "soup-kitchen.library.strict"
-end)
-
 describe("Settings", function()
   require "soup-kitchen.settings"
+  local settings
+
+  before_each(function ()
+    require "soup-kitchen.library.strict"
+    settings = Settings()
+  end)
 
   it("performs basic initialization", function()
-    local settings = Settings()
+    assert.is_table(settings.Magic)
+    assert.is_table(settings.Graphics)
+    assert.is_table(settings.Scenes)
+    assert.is_table(settings.Map)
   end)
 end)
