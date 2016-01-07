53dbd66aa5370fe3173486a9c298e13994600844
Bryan DeGrendel <bryan@degrendel.com>
2016-01-07 02:13:38 -0500

    Added basic Coordinate tests

---
+++ tests/world/coordinate_spec.lua
@@ -0,0 +1,46 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2016
+
+require "tests.setup"
+
+describe("coordinate", function()
+  require "soup-kitchen.settings"
+  require "soup-kitchen.world.map"
+  require "soup-kitchen.world.coordinate"
+
+  local settings, map 
+  local a, b
+  local x, y
+  before_each(function()
+    settings = Settings()
+    map = Map(settings)
+  end)
+
+  it("performs basic initialization", function()
+    x, y = settings.Map.Dimensions.Width - 2, settings.Map.Dimensions.Height - 2
+    a = Coordinate(settings, x, y)
+    assert.are.equal(x, a.X)
+    assert.are.equal(y, a.Y)
+  end)
+
+  it("accepts inbounds edge values", function()
+    x, y = 0, 0
+    a = Coordinate(settings, x, y)
+    assert.are.equal(x, a.X)
+    assert.are.equal(y, a.Y)
+
+    x, y = settings.Map.Dimensions.Width - 1, settings.Map.Dimensions.Height - 1
+    a = Coordinate(settings, x, y)
+    assert.are.equal(x, a.X)
+    assert.are.equal(y, a.Y)
+  end)
+
+  it("rejects out of bounds edge values", function()
+    x, y = settings.Map.Dimensions.Width, settings.Map.Dimensions.Height
+    assert.has_error(function() Coordinate(settings, -1,  0) end)
+    assert.has_error(function() Coordinate(settings,  0, -1) end)
+    assert.has_error(function() Coordinate(settings,  0,  y) end)
+    assert.has_error(function() Coordinate(settings,  x,  0) end)
+  end)
+
+end)
