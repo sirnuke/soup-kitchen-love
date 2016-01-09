1423a8d77bc25f275260203b102f6f8ac31827c5
Bryan DeGrendel <bryan@degrendel.com>
2016-01-08 23:47:24 -0500

    Updated the test setup script

--- tests/setup.lua
+++ tests/setup.lua
@@ -1,6 +1,7 @@
 -- Soup Kitchen
 -- Bryan DeGrendel (c) 2016
 
-Inspect = require "soup-kitchen.library.inspect"
-Class = require "soup-kitchen.library.30log"
-Log = require "soup-kitchen.log"
+Inspect = require "library.inspect"
+Class = require "library.30log"
+Utilities = require "utilities"
+Log = require "log"
