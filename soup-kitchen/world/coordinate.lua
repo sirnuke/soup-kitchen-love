079f57b7ef91477b96355c42989ef64daac54f4c
Bryan DeGrendel <bryan@degrendel.com>
2016-01-08 23:47:35 -0500

    Coordinate now checks for non integer arguments

--- soup-kitchen/world/coordinate.lua
+++ soup-kitchen/world/coordinate.lua
@@ -7,9 +7,10 @@ Coordinate = Class("Position")
 
 function Coordinate:init(settings, x, y)
   assert(Class.isInstance(settings, Settings))
-  assert(type(x) == "number" and x >= 0 and x < settings.Map.Dimensions.Width,
+  assert(Utilities:isInteger(x) and Utilities:isInteger(y))
+  assert(x >= 0 and x < settings.Map.Dimensions.Width,
     string.format("x:%i is out of range [0..%i)", x, settings.Map.Dimensions.Width))
-  assert(type(y) == "number" and y >= 0 and y < settings.Map.Dimensions.Height,
+  assert(y >= 0 and y < settings.Map.Dimensions.Height,
     string.format("y:%i is out of range [0..%i)", y, settings.Map.Dimensions.Height))
   self.Settings = settings
   self.X = x
