d147a57265a2b6089ef724c6640f60323ebcb66d
Bryan DeGrendel <bryan@degrendel.com>
2016-06-18 22:58:42 -0400

    Added placeholder tests for Object and Pawn

---
+++ tests/world/object_spec.lua
@@ -0,0 +1,7 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2016
+
+require "tests.setup"
+
+describe("object", function()
+end)
---
+++ tests/world/pawn_spec.lua
@@ -0,0 +1,8 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2016
+
+require "tests.setup"
+
+describe("pawn", function()
+end)
+
