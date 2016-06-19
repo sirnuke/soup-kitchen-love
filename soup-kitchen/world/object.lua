e709295def7b1c01c1ba05fabe32c675effe1b39
Bryan DeGrendel <bryan@degrendel.com>
2016-06-18 22:50:23 -0400

    Added stub classes for World's Object and Pawn

---
+++ soup-kitchen/world/object.lua
@@ -0,0 +1,7 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2016
+
+Object = Class("Object")
+
+function Object:init(position)
+end
---
+++ soup-kitchen/world/pawn.lua
@@ -0,0 +1,8 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2016
+
+Pawn = Class("Pawn")
+
+function Pawn:init(position)
+end
+
