6973fe5d3dd4061d8d3c7136ed5978711ec77d99
Bryan DeGrendel <bryan@degrendel.com>
2016-06-19 17:17:09 -0400

    Implemented Object tracking in Tile

--- soup-kitchen/world/tile.lua
+++ soup-kitchen/world/tile.lua
@@ -7,5 +7,16 @@ function Tile:init(coordinate)
   assert(Class.isInstance(coordinate, Coordinate))
   self.Coordinate = coordinate:duplicate()
   self.Object = nil
-  self.Pawn = nil
+  self.Blocked = false
+end
+
+function Tile:setObject(object, blocked)
+  assert(Class.isInstance(object, Object))
+  assert(type(blocked) == "boolean")
+  self.Object = object
+  self.Blocked = blocked
+end
+
+function Tile:isBlocked()
+  return self.Blocked
 end
