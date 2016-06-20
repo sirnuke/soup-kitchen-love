2d9e6454ad0c620f004fb5ce8e0d2e6135c5351b
Bryan DeGrendel <bryan@degrendel.com>
2016-06-19 21:12:33 -0400

    Stub draw for Object

--- soup-kitchen/objects/object.lua
+++ soup-kitchen/objects/object.lua
@@ -4,4 +4,14 @@
 Object = Class("Object")
 
 function Object:init(position)
+  self.OverrideWarn = {
+    Draw = false,
+  }
+end
+
+function Object:draw(coordinate)
+  if not self.OverrideWarn.Draw then
+    Log.warn(tag, "Object %s doesn't override :draw()!", self.name)
+    self.OverrideWarn.Draw = true
+  end
 end
