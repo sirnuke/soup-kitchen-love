810c3392a805b225a4dab87d65fff57e7c1c1191
Bryan DeGrendel <bryan@degrendel.com>
2016-06-19 21:10:52 -0400

    Added test for iterating over a map

--- tests/world/map_spec.lua
+++ tests/world/map_spec.lua
@@ -6,10 +6,10 @@ require "tests.setup"
 describe("Map", function()
   require "soup-kitchen.settings"
   require "soup-kitchen.world.map"
-  local map
+  local map, data
+  local x, y
 
   before_each(function ()
-    require "soup-kitchen.library.strict"
     map = Map()
   end)
 
@@ -18,6 +18,25 @@ describe("Map", function()
     assert.are.not_equal(Settings.Map.Tile, {})
     assert.are.not_equal(Settings.Map.Tile.Dimensions, {})
     assert.are.not_equal(Settings.Map.Dimensions, {})
+  end)
+
+  it("creates right number of tiles and iterates", function()
+    data = {}
+    map:eachTile(function(tile)
+      assert.is_true(Class.isInstance(tile, Tile))
+      x = tile.Coordinate.X + 1
+      y = tile.Coordinate.Y + 1
+      if data[y] == nil then data[y] = {} end
+      assert.is_nil(data[y][x])
+      data[y][x] = true
+    end)
+    assert.are.equal(#data, Settings.Map.Dimensions.Height)
+    for y = 1, Settings.Map.Dimensions.Height do
+      assert.are.equal(#data[y], Settings.Map.Dimensions.Width)
+      for x = 1, Settings.Map.Dimensions.Width do
+        assert.is_true(data[y][x])
+      end
+    end
   end)
 end)
 
