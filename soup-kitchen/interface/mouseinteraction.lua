58e2defd716438ae170ba3482ad95a6cc5ef0d4b
Bryan DeGrendel <bryan@degrendel.com>
2016-08-12 00:03:31 -0400

    Better MouseInteraction functionality

--- soup-kitchen/interface/mouseinteraction.lua
+++ soup-kitchen/interface/mouseinteraction.lua
@@ -4,26 +4,43 @@
 MouseInteraction = Class("MouseInteraction")
 
 function MouseInteraction:init(listener, x, y, w, h)
-  assert(type(listener) ~= "nil", type(x) == "number", type(y) == "number", type(w) == "number",
-    type(h) == "number", "Valid arguments")
+  assert(type(listener) ~= "nil", "Valid arguments")
   self.listener = listener
   self.clicked = false
+  self:setBounds(x, y, w, h)
+end
+
+function MouseInteraction:setBounds(x, y, w, h)
+  assert(type(x) == "number", type(y) == "number", type(w) == "number", type(h) == "number",
+    "Valid arguments")
   self.X1 = x
   self.Y1 = y
   self.X2 = x + w
   self.Y2 = y + h
 end
 
+function MouseInteraction:inBounds(x, y)
+  if x >= self.X1 and y >= self.Y1 and x < self.X2 and y < self.Y2 then
+    return true
+  else
+    return false
+  end
+end
+
+function MouseInteraction:translate(x, y)
+  return x - self.X1, y - self.Y1
+end
+
 function MouseInteraction:press(x, y)
   if self:inBounds(x, y) then
     self.clicked = true
-    self.listener:mousePress(x - self.X1, y - self.Y1)
+    self.listener:mousePress(self:translate(x, y))
   end
 end
 
 function MouseInteraction:hover(x, y)
   if self:inBounds(x, y) then
-    self.listener:mouseHover(x - self.X1, y - self.Y1)
+    self.listener:mouseHover(self:translate(x, y))
   else
     self.clicked = false
     self.listener:mouseClear()
@@ -32,15 +49,8 @@ end
 
 function MouseInteraction:release(x, y)
   if self:inBounds(x, y) and self.clicked then
-    self.listener:mouseTrigger(x - self.X1, y - self.Y1)
+    self.listener:mouseTrigger(self:translate(x, y))
   end
   self.clicked = false
 end
 
-function MouseInteraction:inBounds(x, y)
-  if x >= self.X1 and y >= self.Y1 and x < self.X2 and y < self.Y2 then
-    return true, x - self.X1, y - self.Y1
-  else
-    return false, nil, nil
-  end
-end
