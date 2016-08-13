8591dd4dc8688a7595497d1cf2fc551a72fdd48b
Bryan DeGrendel <bryan@degrendel.com>
2016-08-13 18:34:56 -0400

    Better Tile tests

--- tests/world/tile_spec.lua
+++ tests/world/tile_spec.lua
@@ -17,14 +17,22 @@ describe("Tile", function()
 
   before_each(function ()
     map = Map()
-  end)
-
-  it("requires a valid parameters", function()
     x, y = Settings.Map.Dimensions.Width - 2, Settings.Map.Dimensions.Height - 2
     coord = Coordinate(x, y)
+  end)
+
+  it("requires valid parameters", function()
     assert.has_error(function() Tile(nil, coord) end)
     assert.has_error(function() Tile("invalid", coord) end)
-    assert.has_error(function() Tile("Floor") end)
-    Tile("Floor", coord)
+    assert.has_error(function() Tile(Settings.Map.Tile.Default) end)
+    Tile(Settings.Map.Tile.Default, coord)
+  end)
+
+  it("updates the tile id", function()
+    tile = Tile(Settings.Map.Tile.Default, coord)
+    assert.has_error(function() tile:update("invalid") end)
+    assert.equals(tile.Id, Settings.Map.Tile.Default)
+    tile:update("Blocked")
+    assert.not_equals(tile.Id, Settings.Map.Tile.Default)
   end)
 end)
