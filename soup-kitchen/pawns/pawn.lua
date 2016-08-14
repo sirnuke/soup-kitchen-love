b0d003e098c3618cc0e85ef5718204f78518c5c0
Bryan DeGrendel <bryan@degrendel.com>
2016-08-13 21:04:24 -0400

    Added tracking destination for a Pawn

--- soup-kitchen/pawns/pawn.lua
+++ soup-kitchen/pawns/pawn.lua
@@ -6,6 +6,7 @@ Pawn = Class("Pawn")
 function Pawn:init(id, position)
   assert(Class.isInstance(position, Position))
   self.Position = position:duplicate()
+  self.Destination = nil
   self:update(id)
 end
 
@@ -13,5 +14,10 @@ function Pawn:update(id)
   assert(Settings.Map.Pawns[id] ~= nil, "Must have valid pawn id")
   self.Controllable = Settings.Map.Pawns[id].Controllable
   self.Id = id
+end
+
+function Pawn:setDestination(coordinate)
+  assert(Class.isInstance(coordinate, Coordinate))
+  self.Destination = coordinate:duplicate()
 end
 
